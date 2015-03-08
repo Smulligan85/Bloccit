df148c691a63cdfb2f1377ab05da484ae80c37b5
Sean Mulligan <sean.mulligan85@gmail.com>
2015-03-08 13:34:56 -0700

    Post markdown assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,22 @@ class Post < ActiveRecord::Base
   validates :user, presence: true
   validates :topic, presence: true
 
+  def markdown_title
+    self.render_as_markdown
+  end
+
+  def markdown_body
+    self.render_as_markdown
+  end
+
+
+  private
+
+  def render_as_markdown(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
 end
