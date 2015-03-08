2c298ce7a73c82103cf98af0cdb69e41a00094c0
Sean Mulligan <sean.mulligan85@gmail.com>
2015-03-07 17:59:36 -0800

    Associations assignment complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   default_scope { order('created_at DESC') }
+
+  scope :ordered_by_title, -> { where(ordered_by_title: true) }
+  scope :ordered_by_reverse_created_at, -> { where(ordered_by_reverse_created_at: true) }
 end
