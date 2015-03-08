fc6e48436cacd3f6050c76d5b0ce434cb51ac998
Sean Mulligan <sean.mulligan85@gmail.com>
2015-03-07 16:06:01 -0800

    Adding User Post associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
 
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
 50.times do
   
   Post.create!(
+      user: users.sample,
       title: Faker::Lorem.sentence,
       body: Faker::Lorem.paragraph
     )
@@ -20,11 +33,20 @@ posts = Post.all
 100.times do
 
   Comment.create!(
+      # user: users.sample,   # we have not yet associated Users with Comments
       post: posts.sample,
       body: Faker::Lorem.paragraph
     )
 end
 
+user = User.first
+user.skip_confirmation!
+user.update_attributes!(
+    email:    'sean.mulligan85@gmail.com',
+    password: 'helloworld'
+  )
+
 puts "Seeds finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts were created."
 puts "#{Comment.count} comments were created."
