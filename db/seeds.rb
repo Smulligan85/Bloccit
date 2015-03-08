c83625e4bbed76811d362e32392ea76c9d61943c
Sean Mulligan <sean.mulligan85@gmail.com>
2015-03-07 20:49:45 -0800

    Associate and nest topics and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,22 @@ end
 
 users = User.all
 
+15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+
+topics = Topic.all
+
 50.times do
   
   Post.create!(
-      user: users.sample,
-      title: Faker::Lorem.sentence,
-      body: Faker::Lorem.paragraph
+    user: users.sample,
+    topic: topics.sample,
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
     )
 end
 
@@ -39,12 +49,34 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.skip_confirmation!
-user.update_attributes!(
-    email:    'sean.mulligan85@gmail.com',
-    password: 'helloworld'
+# Create a admin
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
   )
+admin.skip_confirmation!
+admin.save!
+
+# Create a moderator
+moderator = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  role: 'moderator'
+  )
+moderator.skip_confirmation!
+moderator.save!
+
+# Create a member
+member = User.new(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld'
+  )
+member.skip_confirmation!
+member.save!
 
 puts "Seeds finished"
 puts "#{User.count} users created"
